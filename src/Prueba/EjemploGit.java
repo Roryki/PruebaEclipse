726840e62ae5ab8b4c8a2af34cc8a11fe894bcf3
DAW1-B <DAW1-B@pc>
2024-03-05 17:39:24 +0100

    Commit de los cambios realizados en la prueba

---
+++ src/Prueba/EjemploGit.java
@@ -0,0 +1,11 @@
+package Prueba;
+
+public class EjemploGit {
+	
+	public static void main(String[] args) {
+		System.out.println("Esto es una prueba para Git");
+		System.out.println("No sé que cambios realizar");
+		System.out.println("Supongo que esto está bien");
+	}
+	
+}
